a5dc25273e8eda1992506f839afc6d6064c6c94b
Viplav jha <viplavjha1993@gmail.com>
2023-09-16 12:34:31 +0530

    Error handler logics

--- middleware/error.ts
+++ middleware/error.ts
@@ -1,3 +1,39 @@
-import ErrorHandler from "../utils/ErrorHandler";
+import { NextFunction,Request,Response } from "express";
+import Errorhandler from "../utils/ErrorHandler";
 
-module.exports ={}
+
+module.exports =(err:any,req:Request,res:Response, next:NextFunction) =>{
+   err.statusCode =err.statusCode || 500;
+   err.message = err.message || 'internal server error'
+
+
+   // wrong mongodb id Error
+ if(err.name === 'CasteError'){
+    const message =`Resource not found. Invaild: ${err.path}`;
+    err =new Errorhandler(message,400);
+ }
+
+  //Duplicate key error
+  if(err.code ===11000){
+  const message =`Duplicate ${Object.keys(err.keyValue)} entered`;
+  err =new Errorhandler(message,400)
+  }
+  
+  // wrong jwt error
+  if(err.name ===  'jsonwebtokenError'){
+    const message = `Json web token is invalid, try again`
+    err = new Errorhandler(message,400)
+  }
+
+  //JWT  expired error
+  if(err.name === 'TokenExpiredError') {
+    const message = `Json web token is expired , try again`
+    err = new Errorhandler(message,400)
+  }
+  res.status(err.statusCode).json({
+    success:false,
+    message:err.message
+  })
+}
+
+
