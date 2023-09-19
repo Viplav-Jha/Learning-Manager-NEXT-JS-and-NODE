0e2c4c5f070d650aded39adeb06d067a042786c9
Viplav jha <viplavjha1993@gmail.com>
2023-09-19 13:24:37 +0530

    Acccess tokens logics fixes

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -12,49 +12,59 @@ interface ITokenOptions {
 }
 
 export const sendToken =(user:IUser,statusCode:number,res:Response) =>{
-    const accessToken =user.SignAccessToken();
-    const refreshToken =user.SignRefreshToken();
 
-    //upload session to radis 
-     redis.set(user._id,JSON.stringify(user) as any );
-
-    //parse environment variable to intergrates with fallback values
-    const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || '300' ,10)
-    const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '1200' ,10)
-    console.log("----accessTokenExpire-------",accessTokenExpire);
-    console.log("----refreshTokenExpire-------",refreshTokenExpire)
-
-    //options for cookies
-    const accessTokenOptions: ITokenOptions ={
-        expire: new Date(Date.now() + accessTokenExpire * 1000),
-        maxAge: accessTokenExpire * 1000,
-        httpOnly: true,
-        sameSite: 'lax',
-        secure: false
-    };
-    console.log("----accessTokenOptions-------",accessTokenOptions)
-
-    const refreshTokenOptions: ITokenOptions ={
-        expire: new Date(Date.now() + refreshTokenExpire * 1000),
-        maxAge: refreshTokenExpire * 1000,
-        httpOnly: true,
-        sameSite: 'lax',
-        secure: false
-    };
-
-    //only set secure to true in production
-
-    if(process.env.NODE_ENV =='production') {
-        accessTokenOptions.secure =true
-    }
-
-    res.cookie("access_token",accessToken, accessTokenOptions);
-    res.cookie("refresh_token",refreshToken,refreshTokenOptions);
+    try {
+        const accessToken =user.SignAccessToken();
+        const refreshToken =user.SignRefreshToken();
     
-    res.status(statusCode).json({
-        success:true,
-        user,
-        accessToken,
-    })
+        console.log("Access Token:", accessToken);
+        console.log("Refresh Token:", refreshToken);
+    
+        //upload session to radis 
+         redis.set(user._id,JSON.stringify(user) as any );
+    
+        //parse environment variable to intergrates with fallback values
+        const accessTokenExpire = parseInt(process.env.ACCESS_TOKEN_EXPIRE || '300' ,10)
+        const refreshTokenExpire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '1200' ,10)
+        console.log("----accessTokenExpire-------",accessTokenExpire);
+        console.log("----refreshTokenExpire-------",refreshTokenExpire)
+    
+        //options for cookies
+        const accessTokenOptions: ITokenOptions ={
+            expire: new Date(Date.now() + accessTokenExpire * 1000),
+            maxAge: accessTokenExpire * 1000,
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: false
+        };
+        console.log("----accessTokenOptions-------",accessTokenOptions)
+    
+        const refreshTokenOptions: ITokenOptions ={
+            expire: new Date(Date.now() + refreshTokenExpire * 1000),
+            maxAge: refreshTokenExpire * 1000,
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: false
+        };
+    
+        //only set secure to true in production
+    
+        if(process.env.NODE_ENV =='production') {
+            accessTokenOptions.secure =true
+        }
+    
+        res.cookie("access_token",accessToken, accessTokenOptions);
+        res.cookie("refresh_token",refreshToken,refreshTokenOptions);
+        
+        res.status(statusCode).json({
+            success:true,
+            user,
+            accessToken,
+        })
+    
 
+    }catch(error){
+        console.error("Error in sendToken:", error);
+    }
+   
 }
